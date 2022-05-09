059ec668cba3321447d1f35e3679ea3b368a81e7
srikant21 <srikant21.k.sahoo@gmail.com>
2022-05-09 16:14:31 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,19 @@
-module.exports = function getStatistics(numbers){
-    console.log(numbers);
+module.exports = function getStatistics(numArray:Array){
+    Array.prototype.max = function(){
+        return Math.max.apply(null,this);
+    }
+     Array.prototype.min = function(){
+        return Math.min.apply(null,this);
+    }
+    
+     Array.prototype.avg = function(){
+        var arrSum = 0;
+        for(var count = 0;count<numArray.length;count++){
+            arrSum += numArray[count];
+        }
+        var avgSum =  arrSum/numArray.length;
+        return avgSum;
+    }
     //implement the computation of statistics here
 }
 
