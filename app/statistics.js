4ced1b24000336136ab2ff7d5d6b286fc9b86ac1
srikant21 <srikant21.k.sahoo@gmail.com>
2022-05-10 14:20:52 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -1,19 +1,51 @@
-module.exports = function getStatistics(numArray:Array){
-    Array.prototype.max = function(){
-        return Math.max.apply(null,this);
-    }
-     Array.prototype.min = function(){
-        return Math.min.apply(null,this);
-    }
-    
-     Array.prototype.average = function(){
-        var arrSum = 0;
-        for(var count = 0;count<numArray.length;count++){
-            arrSum += numArray[count];
+function getStatistics(numArray){
+    if (!numArray.some(isNaN) && numArray.length > 0) {
+        arrMax = () => {
+            return Math.max.apply(null, numArray);
         }
-        var avgSum =  arrSum/numArray.length;
-        return avgSum;
+        arrMin = () => {
+            return Math.min.apply(null, numArray);
+        }
+        
+        arrAverage = () => {
+            var arrSum = 0;
+            for(var count = 0;count<numArray.length;count++){
+                arrSum += numArray[count];
+            }
+            var avgSum =  arrSum/numArray.length;
+            return avgSum;
+        }
+        return {min:arrMin(),max:arrMax(),average:arrAverage()}
+    }else{
+        return {min:NaN,max:NaN,average:NaN}
     }
     //implement the computation of statistics here
 }
 
+function EmailAlert() {
+    emailSent: true;
+}
+
+
+function LEDAlert() {
+    ledGlows: true;
+}
+
+class StatsAlerter {
+
+    constructor(mThreshold, alerts) {
+        this.threshold = mThreshold;
+        [this.emailAlert, this.ledAlert] = alerts;
+    }
+
+    checkAndAlert(numArray) {
+        let checkMaxThreshold = numArray.some(element => element > this.threshold);
+        if (checkMaxThreshold) {
+            this.emailAlert.emailSent = true;
+            this.ledAlert.ledGlows = true;
+        }
+    };
+
+}
+
+module.exports = {getStatistics,EmailAlert,LEDAlert,StatsAlerter}
